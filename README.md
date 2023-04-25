97cb3af7e4fbb72b568bee5915007df587b78d9e
Web Developer <useraccount@WebDeveloperMBP.local>
2023-04-25 13:31:21 -0500

    V1

---
+++ README.md
@@ -0,0 +1,3 @@
+# ProductCard
+
+## What this App does
---
+++ react/ProductCard.tsx
@@ -0,0 +1,87 @@
+import React, { ReactChildren, useRef, useState, useEffect } from "react";
+import { Link } from "vtex.render-runtime";
+
+// Styles
+import styles from "./styles.css";
+
+interface ProductCardProps {
+  productTitle: string
+  children: ReactChildren | any
+  image: ImageObject
+  link: LinkObject
+  fadeIn: boolean
+}
+
+interface ImageObject {
+  src: string
+  width: number
+  height: number
+  alt: string
+}
+
+interface LinkObject {
+  href: string
+  text: string
+}
+
+const ProductCard: StorefrontFunctionComponent<ProductCardProps> = ({ productTitle, children, image, link, fadeIn }) => {
+  const observer = useRef<IntersectionObserver>();
+  const cardRef: any = useRef();
+  const [cardOpacity, setCardOpacity] = useState(fadeIn === false ? 1 : 0.01);
+  const [cardScale, setCardScale] = useState(fadeIn === false ? 1 : 0.90);
+
+  // Create / Destroy Intersection Observer
+  useEffect(() => {
+    if (!window) return;
+    const reducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)');
+
+    if (reducedMotion.matches) {
+      setCardOpacity(1);
+      setCardScale(1);
+      return;
+    }
+
+    if (fadeIn === false) return;
+
+    observer.current = new IntersectionObserver(entries => {
+      const entry: IntersectionObserverEntry = entries[0];
+      if (entry.isIntersecting) animateIn();
+    }, { threshold: 0.1, rootMargin: `0px` });
+
+    observer.current.observe(cardRef.current);
+
+    return () => {
+      if (observer.current) observer.current.unobserve(cardRef.current);
+    }
+  });
+
+  const animateIn = () => {
+    setCardOpacity(1);
+    setCardScale(1);
+    if (observer.current) observer.current.unobserve(cardRef.current);
+  }
+
+  const kebob = (title: string) => title.replace(" ", "-").toLocaleLowerCase();
+
+  return (
+    <section ref={cardRef} id={`${kebob(productTitle)}-card`} style={{ opacity: cardOpacity, transform: `scale(${cardScale})` }} className={styles.productCard} aria-labelledby={`${kebob(productTitle)}-title`}>
+      <img src={image.src} width={image.width || 450} height={image.height || 338} className={styles.cardImage} loading={fadeIn === false ? "eager" : "lazy"} />
+      <div className={styles.textContainer}>
+        <h2 id={`${kebob(productTitle)}-title`} className={styles.productTitle}>{productTitle}</h2>
+        {children}
+        {link.href && <Link href={link.href} className={styles.cardButton} >{link.text || `Shop The ${productTitle}`}</Link>}
+      </div>
+    </section>
+  );
+}
+
+ProductCard.schema = {
+  title: "ProductCard",
+  description: "",
+  type: "object",
+  properties: {
+
+  }
+}
+
+export default ProductCard;
